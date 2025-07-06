4c1aeb95844645c5bfce6b5042fa6908abe839e4
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-07-07 00:46:12 +0530

    Update main.py to return styled HTML page with version label using Jinja2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI Blue-Green Deployment!"}
+# Mount static folder for CSS
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Templates folder
+templates = Jinja2Templates(directory="app/templates")
+
+# ✅ Update this per deployment
+CURRENT_DEPLOYMENT = "green"
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "version": CURRENT_DEPLOYMENT})
